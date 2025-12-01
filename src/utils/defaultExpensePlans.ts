6d0ca971b95907f5854ddf8475045a0f7fd69372
wo-ong-dev <wjwj9509@gmail.com>
2025-12-02 01:31:26 +0900

    chore: sync default expense plan template with 2025-11 data

--- src/utils/defaultExpensePlans.ts
+++ src/utils/defaultExpensePlans.ts
@@ -4,39 +4,39 @@ import type { ExpensePlanDraft } from "../types/expensePlan";
 // month 값만 현재 선택된 달로 바꿔서 사용한다.
 
 const TEMPLATE_PLANS: Omit<ExpensePlanDraft, "month">[] = [
-  // 국민은행
-  { account: "국민은행", name: "여행계 1", amount: 10_000, due_day: 1 },
-  { account: "국민은행", name: "무제휴비 5", amount: 50_000, due_day: 1 },
-  { account: "국민은행", name: "청년적금 70", amount: 700_000, due_day: 4 },
-  { account: "국민은행", name: "상남금 30", amount: 300_000, due_day: 1 },
-  { account: "국민은행", name: "운전자보험 1", amount: 10_000, due_day: 10 },
-  { account: "국민은행", name: "대출이자 18.5", amount: 185_000, due_day: 10 },
-  { account: "국민은행", name: "보험 14", amount: 140_000, due_day: 10 },
-  { account: "국민은행", name: "상조 4", amount: 40_000, due_day: 10 },
-  { account: "국민은행", name: "보컬레슨 15", amount: 150_000, due_day: 1 },
-  { account: "국민은행", name: "쿠팡와우 0.8", amount: 8_000, due_day: 21 },
-  { account: "국민은행", name: "인터넷 2", amount: 20_000, due_day: 25 },
-  { account: "국민은행", name: "월세 20", amount: 200_000, due_day: 28 },
-  { account: "국민은행", name: "수도/전기 5", amount: 50_000, due_day: 1 },
-  { account: "국민은행", name: "주택적약 25", amount: 250_000, due_day: 30 },
+  // 국민은행 (2025-11 현재 DB 상태 기준)
+  { account: "국민은행", name: "무제 회비", amount: 50_000, due_day: 1 },
+  { account: "국민은행", name: "보컬레슨", amount: 150_000, due_day: 1 },
+  { account: "국민은행", name: "상납금", amount: 300_000, due_day: 1 },
+  { account: "국민은행", name: "수도/전기", amount: 50_000, due_day: 1 },
+  { account: "국민은행", name: "여행계", amount: 10_000, due_day: 1 },
+  { account: "국민은행", name: "대출이자", amount: 185_000, due_day: 10 },
+  { account: "국민은행", name: "보험", amount: 140_000, due_day: 10 },
+  { account: "국민은행", name: "상조", amount: 40_000, due_day: 10 },
+  { account: "국민은행", name: "운전자보험", amount: 10_000, due_day: 10 },
+  { account: "국민은행", name: "청년적금", amount: 700_000, due_day: 10 },
+  { account: "국민은행", name: "쿠팡와우", amount: 8_000, due_day: 21 },
+  { account: "국민은행", name: "인터넷", amount: 20_000, due_day: 25 },
+  { account: "국민은행", name: "월세", amount: 200_000, due_day: 28 },
+  { account: "국민은행", name: "주택청약", amount: 250_000, due_day: 30 },
 
   // 신용카드
-  { account: "신용카드", name: "휴대폰 9", amount: 90_000, due_day: 10 },
-  { account: "신용카드", name: "가스비 10", amount: 100_000, due_day: 26 },
-  { account: "신용카드", name: "교통비 7", amount: 70_000, due_day: 31 },
+  { account: "신용카드", name: "휴대폰", amount: 90_000, due_day: 10 },
+  { account: "신용카드", name: "가스비", amount: 100_000, due_day: 26 },
+  { account: "신용카드", name: "교통비", amount: 70_000, due_day: 31 },
 
   // 토스뱅크
-  { account: "토스뱅크", name: "생활비 40", amount: 400_000, due_day: 1 },
-  { account: "토스뱅크", name: "미용 5", amount: 50_000, due_day: 1 },
+  { account: "토스뱅크", name: "미용", amount: 50_000, due_day: 1 },
+  { account: "토스뱅크", name: "생활비", amount: 400_000, due_day: 1 },
 
   // 카카오페이
-  { account: "카카오페이", name: "넷플릭스 .055", amount: 550, due_day: 1 },
+  { account: "카카오페이", name: "넷플릭스", amount: 7_000, due_day: 1 },
 
   // 카카오뱅크
-  { account: "카카오뱅크", name: "비상금2 0", amount: 0, due_day: 1 },
+  { account: "카카오뱅크", name: "비상금2", amount: 0, due_day: 1 },
 
   // 우리은행
-  { account: "우리은행", name: "데이트 60", amount: 600_000, due_day: 1 },
+  { account: "우리은행", name: "데이트", amount: 600_000, due_day: 1 },
 ];
 
 export function getDefaultExpensePlans(month: string): ExpensePlanDraft[] {
